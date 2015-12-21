newpascal/lazarus

commit 22e9babef34dfe367daef306227b9ade3ac05f57

IDE: use tbsButtonDrop instead of '>>' buttons
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50903 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/exttooldialog.pas
@@ -142,7 +142,8 @@ begin
   MoveUpButton.Caption:=lisUp;
   MoveDownButton.Caption:=lisDown;
 
-  ExtraButton.Caption:=lisMoreSub;
+  ExtraButton.Caption:=lisMore;
+  ExtraButton.Style:=tbsButtonDrop;
   MenuItemClone.Caption:=lisClone;
   MenuItemExport.Caption:=lisDlgExport;
   MenuItemImport.Caption:=lisDlgImport;

ide/lazarusidestrconsts.pas
@@ -126,7 +126,6 @@ resourcestring
   lisLess = 'Less';
   lisMore = 'More';
   lisDlgMore = 'More ...';
-  lisMoreSub = 'More >>';
   lisDefault = 'Default';
   lisClone = 'Clone';
   lisExport = 'Export';
@@ -4459,8 +4458,7 @@ resourcestring
   lisPckEditAddFilesFromFileSystem = 'Add Files from File System';
   lisPckEditAddOtherItems = 'Add other items';
   lisPckEditRemoveSelectedItem = 'Remove selected item';
-  lisUseSub = 'Use >>';
-  lisAddSub = 'Add >>';
+  lisUse = 'Use';
   lisClickToSeeTheChoices = 'Click to see the choices';
   lisPckEditEditGeneralOptions = 'Edit general options';
   lisPkgEdMoreFunctionsForThePackage = 'More functions for the package';

packager/packageeditor.pas
@@ -1906,14 +1906,17 @@ begin
 
   SaveBitBtn    := CreateToolButton('SaveBitBtn', lisMenuSave, lisPckEditSavePackage, 'laz_save', @SaveBitBtnClick);
   CompileBitBtn := CreateToolButton('CompileBitBtn', lisCompile, lisPckEditCompilePackage, 'pkg_compile', @CompileBitBtnClick);
-  UseBitBtn     := CreateToolButton('UseBitBtn', lisUseSub, lisClickToSeeTheChoices, 'pkg_install', nil);
+  UseBitBtn     := CreateToolButton('UseBitBtn', lisUse, lisClickToSeeTheChoices, 'pkg_install', nil);
+  UseBitBtn.Style:=tbsButtonDrop;
   CreateDivider;
-  AddBitBtn     := CreateToolButton('AddBitBtn', lisAddSub, lisClickToSeeTheChoices, 'laz_add', nil);
+  AddBitBtn     := CreateToolButton('AddBitBtn', lisAdd, lisClickToSeeTheChoices, 'laz_add', nil);
+  AddBitBtn.Style:=tbsButtonDrop;
   RemoveBitBtn  := CreateToolButton('RemoveBitBtn', lisRemove, lisPckEditRemoveSelectedItem, 'laz_delete', @RemoveBitBtnClick);
   CreateDivider;
   OptionsBitBtn := CreateToolButton('OptionsBitBtn', lisOptions, lisPckEditEditGeneralOptions, 'pkg_properties', @OptionsBitBtnClick);
   HelpBitBtn    := CreateToolButton('HelpBitBtn', GetButtonCaption(idButtonHelp), lisMenuOnlineHelp, 'menu_help', @HelpBitBtnClick);
-  MoreBitBtn    := CreateToolButton('MoreBitBtn', lisMoreSub, lisPkgEdMoreFunctionsForThePackage, '', nil);
+  MoreBitBtn    := CreateToolButton('MoreBitBtn', lisMore, lisPkgEdMoreFunctionsForThePackage, '', nil);
+  MoreBitBtn.Style:=tbsButtonDrop;
 
   UseBitBtn.DropdownMenu := UsePopupMenu;
   AddBitBtn.DropdownMenu := AddPopupMenu;

ide/frames/compiler_modematrix.lfm
@@ -28,7 +28,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object BMMMoveDownButton: TToolButton
-      Left = 26
+      Left = 24
       Top = 0
       Caption = 'Down'
       OnClick = BMMMoveDownButtonClick
@@ -36,7 +36,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object BMMUndoButton: TToolButton
-      Left = 79
+      Left = 73
       Top = 0
       Caption = 'Undo'
       OnClick = BMMUndoButtonClick
@@ -44,7 +44,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object BMMRedoToolButton: TToolButton
-      Left = 120
+      Left = 110
       Top = 0
       Caption = 'Redo'
       OnClick = BMMRedoToolButtonClick
@@ -52,7 +52,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object BMMDeleteButton: TToolButton
-      Left = 555
+      Left = 505
       Top = 0
       Caption = 'Delete'
       OnClick = BMMDeleteButtonClick
@@ -60,7 +60,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object MoveSepToolButton: TToolButton
-      Left = 69
+      Left = 63
       Height = 25
       Top = 0
       Width = 10
@@ -68,7 +68,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       Style = tbsSeparator
     end
     object DoSepToolButton: TToolButton
-      Left = 159
+      Left = 145
       Height = 25
       Top = 0
       Width = 10
@@ -76,7 +76,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       Style = tbsSeparator
     end
     object SystemEncodingSepToolButton: TToolButton
-      Left = 545
+      Left = 495
       Height = 25
       Top = 0
       Width = 10
@@ -84,22 +84,22 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       Style = tbsSeparator
     end
     object BMMAddOtherButton: TToolButton
-      Left = 319
+      Left = 292
       Top = 0
       Caption = 'Add Other'
       DropdownMenu = BMMAddOtherPopupMenu
       OnClick = BMMAddOtherButtonClick
-      Style = tbsDropDown
+      Style = tbsButtonDrop
     end
     object BMMAddLclWidgetButton: TToolButton
-      Left = 169
+      Left = 155
       Top = 0
       Caption = 'Add LCLWidgetType'
       OnClick = BMMAddLclWidgetButtonClick
-      Style = tbsDropDown
+      Style = tbsButtonDrop
     end
     object LCLMacroSepToolButton: TToolButton
-      Left = 309
+      Left = 282
       Height = 25
       Top = 0
       Width = 10
@@ -107,7 +107,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       Style = tbsSeparator
     end
     object BMMSystemEncodingButton: TToolButton
-      Left = 411
+      Left = 375
       Top = 0
       Caption = 'Use system encoding'
       OnClick = BMMSystemEncodingButtonClick
@@ -115,7 +115,7 @@ object CompOptModeMatrixFrame: TCompOptModeMatrixFrame
       ShowHint = True
     end
     object AddOtherSepToolButton: TToolButton
-      Left = 401
+      Left = 365
       Height = 25
       Top = 0
       Width = 10

ide/projectinspector.lfm
@@ -9,7 +9,7 @@ object ProjectInspectorForm: TProjectInspectorForm
   ClientHeight = 456
   ClientWidth = 299
   OnDropFiles = FormDropFiles
-  LCLVersion = '1.5'
+  LCLVersion = '1.7'
   object ItemsTreeView: TTreeView
     Left = 0
     Height = 378

ide/projectinspector.pas
@@ -997,7 +997,8 @@ begin
   ToolBar.Images            := IDEImages.Images_16;
   FilterEdit.OnGetImageIndex:=@OnTreeViewGetImageIndex;
 
-  AddBitBtn     := CreateToolButton('AddBitBtn', lisAddSub, lisClickToSeeTheChoices, 'laz_add', nil);
+  AddBitBtn     := CreateToolButton('AddBitBtn', lisAdd, lisClickToSeeTheChoices, 'laz_add', nil);
+  AddBitBtn.Style:=tbsButtonDrop;
   RemoveBitBtn  := CreateToolButton('RemoveBitBtn', lisRemove, lisPckEditRemoveSelectedItem, 'laz_delete', @RemoveBitBtnClick);
   CreateDivider;
   OptionsBitBtn := CreateToolButton('OptionsBitBtn', lisOptions, lisPckEditEditGeneralOptions, 'menu_environment_options', @OptionsBitBtnClick);

commit 4169d3c9cc0421c3d019dcbaf8432c362794db0a

IDE: fixed i18n regression after r50903
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50968 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/exttooldialog.pas
@@ -142,7 +142,7 @@ begin
   MoveUpButton.Caption:=lisUp;
   MoveDownButton.Caption:=lisDown;
 
-  ExtraButton.Caption:=lisMore;
+  ExtraButton.Caption:=lisMoreSub;
   ExtraButton.Style:=tbsButtonDrop;
   MenuItemClone.Caption:=lisClone;
   MenuItemExport.Caption:=lisDlgExport;

ide/lazarusidestrconsts.pas
@@ -125,6 +125,7 @@ resourcestring
   lisOptions = 'Options';
   lisLess = 'Less';
   lisMore = 'More';
+  lisMoreSub = 'More';
   lisDlgMore = 'More ...';
   lisDefault = 'Default';
   lisClone = 'Clone';

packager/packageeditor.pas
@@ -1915,7 +1915,7 @@ begin
   CreateDivider;
   OptionsBitBtn := CreateToolButton('OptionsBitBtn', lisOptions, lisPckEditEditGeneralOptions, 'pkg_properties', @OptionsBitBtnClick);
   HelpBitBtn    := CreateToolButton('HelpBitBtn', GetButtonCaption(idButtonHelp), lisMenuOnlineHelp, 'menu_help', @HelpBitBtnClick);
-  MoreBitBtn    := CreateToolButton('MoreBitBtn', lisMore, lisPkgEdMoreFunctionsForThePackage, '', nil);
+  MoreBitBtn    := CreateToolButton('MoreBitBtn', lisMoreSub, lisPkgEdMoreFunctionsForThePackage, '', nil);
   MoreBitBtn.Style:=tbsButtonDrop;
 
   UseBitBtn.DropdownMenu := UsePopupMenu;