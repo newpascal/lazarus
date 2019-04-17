newpascal/lazarus

commit 1cd1a62eb4a5799f55ecf31755bf8cd4a6f4873c

IDE: Added EnvironmentOptions.FppkgConfigFile IDE: Fixed some minor problems in the generation of the fppkg.cfg file
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@60944 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/environmentopts.pp
@@ -283,7 +283,8 @@ type
     eopFPDocPaths,
     eopCompilerMessagesFilename,
     eopDebuggerFilename,
-    eopDebuggerSearchPath
+    eopDebuggerSearchPath,
+    eopFppkgConfigFile
     );
   TEnvOptParseTypes = set of TEnvOptParseType;
 
@@ -646,6 +647,7 @@ type
     FActiveDesktopName: string;
     FAutoSaveActiveDesktop: Boolean;
     FDebugDesktopName: string;
+    FFppkgConfigFileHistory: TStringList;
 
     function GetActiveDesktop: TDesktopOpt;
     function GetCompilerFilename: string;
@@ -662,6 +664,7 @@ type
     function GetMsgViewColors(c: TMsgWndColor): TColor;
     function GetSubConfig(Index: Integer): TIDESubOptions;
     function GetTestBuildDirectory: string;
+    function GetFppkgConfigFile: string;
     procedure LoadNonDesktop(Path: String);
     procedure SaveNonDesktop(Path: String);
     procedure SetCompilerFilename(const AValue: string);
@@ -674,6 +677,7 @@ type
     procedure SetDebuggerFilename(AValue: string);
     procedure SetFPCSourceDirectory(const AValue: string);
     procedure SetLazarusDirectory(const AValue: string);
+    procedure SetFppkgConfigFile(AValue: string);
     procedure SetMsgColors(u: TMessageLineUrgency; AValue: TColor);
     procedure SetMsgViewColors(c: TMsgWndColor; AValue: TColor);
     procedure SetParseValue(o: TEnvOptParseType; const NewValue: string);
@@ -711,6 +715,7 @@ type
     function GetParsedFPDocPaths: string;
     function GetParsedDebuggerFilename: string;
     function GetParsedDebuggerSearchPath: string;
+    function GetParsedFppkgConfig: string; override;
     function GetParsedValue(o: TEnvOptParseType): string;
 
     // macros
@@ -824,6 +829,8 @@ type
     property DebuggerShowStopMessage: boolean read FDebuggerShowStopMessage write FDebuggerShowStopMessage;
     property DebuggerResetAfterRun: boolean read FDebuggerResetAfterRun write FDebuggerResetAfterRun;
     property DebuggerAutoCloseAsm: boolean read FDebuggerAutoCloseAsm write FDebuggerAutoCloseAsm;
+    property FppkgConfigFile: string read GetFppkgConfigFile write SetFppkgConfigFile;
+    property FppkgConfigFileHistory: TStringList read FFppkgConfigFileHistory write FFppkgConfigFileHistory;
     // ShowCompileDialog and AutoCloseCompileDialog are currently not used.
     // But maybe someone will implement them again. Keep them till 1.4.2
     property ShowCompileDialog: boolean read  FShowCompileDialog write FShowCompileDialog;
@@ -1000,7 +1007,8 @@ const
     'FPDocPath', // eopFPDocPaths
     'CompMsgFile', // eopCompilerMessagesFilename
     'Debugger', // eopDebuggerFilename
-    'DebugPath' // eopDebuggerSearchPath
+    'DebugPath', // eopDebuggerSearchPath
+    'FppkgConfig' // eopFppkgConfigFile
   );
 
 function dbgs(o: TEnvOptParseType): string; overload;
@@ -1746,7 +1754,8 @@ begin
   FDebuggerFileHistory:=TStringList.Create;
   FDebuggerProperties := TStringList.Create;
   FDebuggerEventLogColors:=DebuggerDefaultColors;
-
+  FppkgConfigFile:='';
+  FFppkgConfigFileHistory:=TStringList.Create;
   TestBuildDirectory:=GetDefaultTestBuildDirectory;
   FTestBuildDirHistory:=TStringList.Create;
   CompilerMessagesFilename:='';
@@ -2000,6 +2009,8 @@ begin
   LoadRecentList(FXMLCfg,FMakeFileHistory,Path+'MakeFilename/History/',rltFile);
   if FMakeFileHistory.Count=0 then
     GetDefaultMakeFilenames(FMakeFileHistory);
+  FppkgConfigFile:=FXMLCfg.GetValue(Path+'FppkgConfigFile/Value',FppkgConfigFile);
+  LoadRecentList(FXMLCfg,FFppkgConfigFileHistory,Path+'FppkgConfigFile/History/',rltFile);
 
   TestBuildDirectory:=FXMLCfg.GetValue(Path+'TestBuildDirectory/Value',TestBuildDirectory);
   LoadRecentList(FXMLCfg,FTestBuildDirHistory,Path+'TestBuildDirectory/History/',rltFile);
@@ -2395,6 +2406,8 @@ begin
   SaveRecentList(FXMLCfg,FTestBuildDirHistory,Path+'TestBuildDirectory/History/');
   FXMLCfg.SetDeleteValue(Path+'CompilerMessagesFilename/Value',CompilerMessagesFilename,'');
   SaveRecentList(FXMLCfg,FCompilerMessagesFileHistory,Path+'CompilerMessagesFilename/History/');
+  FXMLCfg.SetDeleteValue(Path+'FppkgConfigFile/Value',FppkgConfigFile,'');
+  SaveRecentList(FXMLCfg,FFppkgConfigFileHistory,Path+'FppkgConfigFile/History/');
   SaveRecentList(FXMLCfg,FManyBuildModesSelection,Path+'ManyBuildModesSelection/');
 
   // Primary-config verification
@@ -2807,6 +2820,11 @@ begin
   Result:=GetParsedValue(eopDebuggerSearchPath);
 end;
 
+function TEnvironmentOptions.GetParsedFppkgConfig: string;
+begin
+  Result:=GetParsedValue(eopFppkgConfigFile);
+end;
+
 function TEnvironmentOptions.GetParsedValue(o: TEnvOptParseType): string;
 var
   SpacePos: SizeInt;
@@ -2860,7 +2878,7 @@ begin
         eopFPDocPaths,eopDebuggerSearchPath:
           // search path
           ParsedValue:=TrimSearchPath(ParsedValue,GetParsedLazarusDirectory,true);
-        eopCompilerFilename,eopMakeFilename,eopDebuggerFilename:
+        eopCompilerFilename,eopMakeFilename,eopDebuggerFilename,eopFppkgConfigFile:
           // program
           begin
             ParsedValue:=Trim(ParsedValue);
@@ -3267,6 +3285,11 @@ begin
   Result:=FParseValues[eopLazarusDirectory].UnparsedValue;
 end;
 
+function TEnvironmentOptions.GetFppkgConfigFile: string;
+begin
+  Result:=FParseValues[eopFppkgConfigFile].UnparsedValue;
+end;
+
 function TEnvironmentOptions.GetMakeFilename: string;
 begin
   Result:=FParseValues[eopMakeFilename].UnparsedValue;
@@ -3315,6 +3338,11 @@ end;
 procedure TEnvironmentOptions.SetDebuggerFilename(AValue: string);
 begin
   SetParseValue(eopDebuggerFilename,UTF8Trim(AValue));
+end;
+
+procedure TEnvironmentOptions.SetFppkgConfigFile(AValue: string);
+begin
+  SetParseValue(eopFppkgConfigFile,UTF8Trim(AValue));
 end;
 
 initialization

components/fppkg/src/fppkg_mainfrm.pas
@@ -46,7 +46,7 @@ uses
   pkgFppkg,
   //IDE interface
   {$IFDEF LazarusIDEPackage}
-    PackageIntf, IDECommands, contnrs, fppkg_lpk,
+    PackageIntf, IDECommands, contnrs, fppkg_lpk, IDEOptionsIntf,
   {$ENDIF}
   // Repository handler objects
   fprepos,
@@ -854,7 +854,7 @@ procedure TFppkgForm.LoadFppkgConfiguration;
 var
   i: Integer;
 begin
-  FFPpkg.InitializeGlobalOptions('');
+  FFPpkg.InitializeGlobalOptions(IDEEnvironmentOptions.GetParsedFppkgConfig);
 
   FFPpkg.Options.GlobalSection.Downloader := 'FPC';
 

components/fppkg/src/fppkgworkerthread.pas
@@ -12,6 +12,7 @@ uses
   pkgoptions,
   pkgglobals,
   pkgFppkg,
+  IDEOptionsIntf,
   pkgmessages,
   laz_pkgrepos;
 
@@ -47,7 +48,7 @@ begin
   pkghandler.ClearExecutedAction;
 
   FFPpkg := TpkgFPpkg.Create(Nil);
-  FFPpkg.InitializeGlobalOptions('');
+  FFPpkg.InitializeGlobalOptions(IDEEnvironmentOptions.GetParsedFppkgConfig);
   FFPpkg.Options.GlobalSection.Downloader := 'FPC';
   FFPpkg.InitializeCompilerOptions;
 

components/ideintf/ideoptionsintf.pas
@@ -135,6 +135,7 @@ type
     procedure RemoveFromRecentPackageFiles(const AFilename: string); virtual; abstract;
 
     function GetParsedCompilerFilename: string; virtual; abstract;
+    function GetParsedFppkgConfig: string; virtual; abstract;
 
     procedure AddHandlerAddToRecentOpenFiles(Handler: TOnAddToRecent; const AsFirst: boolean = true); // AsFirst means: first to call
     procedure RemoveHandlerAddToRecentOpenFiles(Handler: TOnAddToRecent);

ide/frames/files_options.lfm
@@ -1,11 +1,11 @@
 object FilesOptionsFrame: TFilesOptionsFrame
   Left = 0
-  Height = 505
+  Height = 657
   Top = 0
   Width = 622
   ChildSizing.LeftRightSpacing = 2
   ChildSizing.TopBottomSpacing = 2
-  ClientHeight = 505
+  ClientHeight = 657
   ClientWidth = 622
   TabOrder = 0
   Visible = False
@@ -15,9 +15,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideLeft.Control = Owner
     AnchorSideTop.Control = Owner
     Left = 2
-    Height = 15
+    Height = 19
     Top = 2
-    Width = 138
+    Width = 176
     Caption = 'MaxRecentOpenFilesLabel'
     ParentColor = False
   end
@@ -25,9 +25,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideLeft.Control = lblCenter
     AnchorSideTop.Control = Owner
     Left = 314
-    Height = 15
+    Height = 19
     Top = 2
-    Width = 146
+    Width = 188
     BorderSpacing.Left = 3
     Caption = 'MaxRecentProjectFilesLabel'
     ParentColor = False
@@ -38,9 +38,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Side = asrBottom
     Left = 2
-    Height = 19
-    Top = 48
-    Width = 195
+    Height = 23
+    Top = 65
+    Width = 249
     BorderSpacing.Top = 6
     Caption = 'OpenLastProjectAtStartCheckBox'
     TabOrder = 2
@@ -51,9 +51,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Side = asrBottom
     Left = 2
-    Height = 19
-    Top = 94
-    Width = 180
+    Height = 23
+    Top = 127
+    Width = 226
     BorderSpacing.Top = 2
     Caption = 'ShowCompileDialogCheckBox'
     OnChange = ShowCompileDialogCheckBoxChange
@@ -64,9 +64,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = AutoCloseCompileDialogCheckBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 142
-    Width = 82
+    Height = 19
+    Top = 183
+    Width = 109
     BorderSpacing.Top = 10
     Caption = 'LazarusDirLabel'
     ParentColor = False
@@ -79,8 +79,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = LazarusDirComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 157
+    Height = 36
+    Top = 202
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -93,11 +93,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = LazarusDirButton
     Left = 2
-    Height = 23
-    Top = 157
+    Height = 36
+    Top = 202
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     TabOrder = 6
     Text = 'LazarusDirComboBox'
   end
@@ -107,11 +107,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = CompilerPathButton
     Left = 2
-    Height = 23
-    Top = 201
+    Height = 36
+    Top = 263
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     TabOrder = 8
     Text = 'CompilerPathComboBox'
   end
@@ -122,8 +122,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = CompilerPathComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 201
+    Height = 36
+    Top = 263
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -135,9 +135,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = LazarusDirComboBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 186
-    Width = 101
+    Height = 19
+    Top = 244
+    Width = 127
     BorderSpacing.Top = 6
     Caption = 'CompilerPathLabel'
     ParentColor = False
@@ -148,11 +148,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = FPCSourceDirButton
     Left = 2
-    Height = 23
-    Top = 245
+    Height = 36
+    Top = 324
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     TabOrder = 10
     Text = 'FPCSourceDirComboBox'
   end
@@ -163,8 +163,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = FPCSourceDirComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 245
+    Height = 36
+    Top = 324
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -176,9 +176,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = CompilerPathComboBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 230
-    Width = 100
+    Height = 19
+    Top = 305
+    Width = 131
     BorderSpacing.Top = 6
     Caption = 'FPCSourceDirLabel'
     ParentColor = False
@@ -188,9 +188,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = FPCSourceDirComboBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 274
-    Width = 81
+    Height = 19
+    Top = 366
+    Width = 102
     BorderSpacing.Top = 6
     Caption = 'MakePathLabel'
     ParentColor = False
@@ -200,9 +200,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = MakePathComboBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 318
-    Width = 91
+    Height = 19
+    Top = 427
+    Width = 120
     BorderSpacing.Top = 6
     Caption = 'TestBuildDirLabel'
     ParentColor = False
@@ -213,11 +213,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = MakePathButton
     Left = 2
-    Height = 23
-    Top = 289
+    Height = 36
+    Top = 385
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     TabOrder = 12
     Text = 'MakePathComboBox'
   end
@@ -228,8 +228,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = MakePathComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 289
+    Height = 36
+    Top = 385
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -242,11 +242,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = TestBuildDirButton
     Left = 2
-    Height = 23
-    Top = 333
+    Height = 36
+    Top = 446
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     TabOrder = 14
     Text = 'TestBuildDirComboBox'
   end
@@ -257,8 +257,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = TestBuildDirComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 333
+    Height = 36
+    Top = 446
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -271,9 +271,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Side = asrBottom
     Left = 32
-    Height = 19
-    Top = 113
-    Width = 206
+    Height = 23
+    Top = 150
+    Width = 257
     BorderSpacing.Left = 30
     Caption = 'AutoCloseCompileDialogCheckBox'
     TabOrder = 5
@@ -283,9 +283,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = TestBuildDirComboBox
     AnchorSideTop.Side = asrBottom
     Left = 2
-    Height = 15
-    Top = 362
-    Width = 153
+    Height = 19
+    Top = 488
+    Width = 197
     Alignment = taRightJustify
     BorderSpacing.Top = 6
     Caption = 'CompilerTranslationFileLabel'
@@ -300,8 +300,8 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideBottom.Control = CompilerTranslationFileComboBox
     AnchorSideBottom.Side = asrBottom
     Left = 597
-    Height = 23
-    Top = 377
+    Height = 36
+    Top = 507
     Width = 23
     Anchors = [akTop, akRight, akBottom]
     Caption = '...'
@@ -316,11 +316,11 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = CompilerTranslationFileButton
     Left = 2
-    Height = 23
-    Top = 377
+    Height = 36
+    Top = 507
     Width = 595
     Anchors = [akTop, akLeft, akRight]
-    ItemHeight = 15
+    ItemHeight = 0
     ParentShowHint = False
     ShowHint = True
     TabOrder = 16
@@ -342,9 +342,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideRight.Control = lblCenter
     AnchorSideRight.Side = asrBottom
     Left = 2
-    Height = 23
-    Top = 19
+    Height = 36
     Hint = 'Value 0 means unlimited.'
+    Top = 23
     Width = 305
     Anchors = [akTop, akLeft, akRight]
     BorderSpacing.Right = 3
@@ -360,9 +360,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideRight.Control = Owner
     AnchorSideRight.Side = asrBottom
     Left = 316
-    Height = 23
-    Top = 19
+    Height = 36
     Hint = 'Value 0 means unlimited.'
+    Top = 23
     Width = 304
     Anchors = [akTop, akLeft, akRight]
     BorderSpacing.Left = 3
@@ -376,9 +376,9 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Control = MultipleInstancesComboBox
     AnchorSideTop.Side = asrCenter
     Left = 2
-    Height = 15
-    Top = 73
-    Width = 121
+    Height = 19
+    Top = 98
+    Width = 152
     Caption = 'MultipleInstancesLabel'
     ParentColor = False
   end
@@ -389,15 +389,63 @@ object FilesOptionsFrame: TFilesOptionsFrame
     AnchorSideTop.Side = asrBottom
     AnchorSideRight.Control = Owner
     AnchorSideRight.Side = asrBottom
-    Left = 131
-    Height = 23
-    Top = 69
-    Width = 489
+    Left = 162
+    Height = 35
+    Top = 90
+    Width = 458
     Anchors = [akTop, akLeft, akRight]
     BorderSpacing.Left = 8
     BorderSpacing.Top = 2
-    ItemHeight = 15
+    ItemHeight = 0
     Style = csDropDownList
     TabOrder = 3
+  end
+  object FppkgConfigurationFileLabel: TLabel
+    AnchorSideLeft.Control = Owner
+    AnchorSideTop.Control = CompilerTranslationFileComboBox
+    AnchorSideTop.Side = asrBottom
+    Left = 2
+    Height = 19
+    Top = 549
+    Width = 191
+    Alignment = taRightJustify
+    BorderSpacing.Top = 6
+    Caption = 'FppkgConfigurationFileLabel'
+    ParentColor = False
+    ParentShowHint = False
+    ShowHint = True
+  end
+  object FppkgConfigurationFileComboBox: TComboBox
+    AnchorSideLeft.Control = Owner
+    AnchorSideTop.Control = FppkgConfigurationFileLabel
+    AnchorSideTop.Side = asrBottom
+    AnchorSideRight.Control = FppkgConfigurationFileButton
+    Left = 2
+    Height = 36
+    Top = 568
+    Width = 595
+    Anchors = [akTop, akLeft, akRight]
+    ItemHeight = 0
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 18
+    Text = 'FppkgConfigurationFileComboBox'
+  end
+  object FppkgConfigurationFileButton: TButton
+    AnchorSideTop.Control = FppkgConfigurationFileComboBox
+    AnchorSideRight.Control = Owner
+    AnchorSideRight.Side = asrBottom
+    AnchorSideBottom.Control = FppkgConfigurationFileComboBox
+    AnchorSideBottom.Side = asrBottom
+    Left = 597
+    Height = 36
+    Top = 568
+    Width = 23
+    Anchors = [akTop, akRight, akBottom]
+    Caption = '...'
+    OnClick = FilesButtonClick
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 19
   end
 end

ide/frames/files_options.pas
@@ -41,7 +41,7 @@ uses
   // IdeIntf
   IDEOptionsIntf, IDEOptEditorIntf, IDEDialogs, IDEUtils,
   // IDE
-  LazarusIDEStrConsts, InputHistory, LazConf, DialogProcs, InitialSetupProc;
+  LazarusIDEStrConsts, InputHistory, LazConf, DialogProcs, InitialSetupProc, Classes;
 
 type
 
@@ -76,6 +76,9 @@ type
     TestBuildDirButton:TButton;
     TestBuildDirComboBox:TComboBox;
     TestBuildDirLabel:TLabel;
+    FppkgConfigurationFileLabel: TLabel;
+    FppkgConfigurationFileComboBox: TComboBox;
+    FppkgConfigurationFileButton: TButton;
     procedure CompilerTranslationFileButtonClick(Sender:TObject);
     procedure FilesButtonClick(Sender: TObject);
     procedure DirectoriesButtonClick(Sender: TObject);
@@ -93,6 +96,8 @@ type
     FOldRealTestDir: string;
     fOldCompilerMessagesFilename: string;
     fOldRealCompilerMessagesFilename: string;
+    fOldFppkcConfigurationFilename: string;
+    fOldRealFppkcConfigurationFilename: string;
     FOldMaxRecentOpenFiles: integer;
     FOldMaxRecentProjectFiles: integer;
     FOldOpenLastProjectAtStart: boolean;
@@ -255,6 +260,7 @@ begin
 
   CompilerPathLabel.Caption:=Format(dlgFpcExecutable,[GetDefaultCompilerFilename]);
   FPCSourceDirLabel.Caption:=dlgFpcSrcPath;
+  FppkgConfigurationFileLabel.Caption:=dlgFppkgConfigurationFile;
   MakePathLabel.Caption:=dlgMakeExecutable;
   with MakePathComboBox.Items do
   begin
@@ -306,6 +312,7 @@ begin
     MakeFilename:=MakePathComboBox.Text;
     TestBuildDirectory:=TestBuildDirComboBox.Text;
     CompilerMessagesFilename:=CompilerTranslationFileComboBox.Text;
+    FppkgConfigFile:=FppkgConfigurationFileComboBox.Text;
   end;
   // check lazarus directory
   if not CheckLazarusDir([mbIgnore,mbCancel]) then exit;
@@ -374,6 +381,13 @@ begin
       CompilerTranslationFileComboBox.Items.Assign(CompilerMessagesFileHistory);
     SetComboBoxText(CompilerTranslationFileComboBox,CompilerMessagesFilename,cstFilename,MaxComboBoxCount);
 
+    // fppkg configuration  file
+    fOldFppkcConfigurationFilename:=FppkgConfigFile;
+    fOldRealCompilerMessagesFilename:=GetParsedFppkgConfig;
+    if FppkgConfigFileHistory.Count>0 then
+      FppkgConfigurationFileComboBox.Items.Assign(FppkgConfigFileHistory);
+    SetComboBoxText(FppkgConfigurationFileComboBox,FppkgConfigFile,cstFilename,MaxComboBoxCount);
+
     // recent files and directories
     FOldMaxRecentOpenFiles := MaxRecentOpenFiles;
     MaxRecentOpenFilesSpin.Value := MaxRecentOpenFiles;
@@ -411,6 +425,8 @@ begin
     TestBuildDirectory:=TestBuildDirComboBox.Text;
     CompilerMessagesFileHistory.Assign(CompilerTranslationFileComboBox.Items);
     CompilerMessagesFilename:=CompilerTranslationFileComboBox.Text;
+    FppkgConfigFileHistory.Assign(FppkgConfigurationFileComboBox.Items);
+    FppkgConfigFile:=FppkgConfigurationFileComboBox.Text;
 
     // recent files and directories
     MaxRecentOpenFiles := MaxRecentOpenFilesSpin.Value;
@@ -433,6 +449,7 @@ begin
     MakeFilename:=FOldMakeFilename;
     TestBuildDirectory:=FOldTestDir;
     CompilerMessagesFilename:=fOldCompilerMessagesFilename;
+    FppkgConfigFile:=fOldFppkcConfigurationFilename;
 
     // recent files and directories
     MaxRecentOpenFiles := FOldMaxRecentOpenFiles;

ide/generatefppkgconfigurationdlg.pas
@@ -1,3 +1,23 @@
+{ Dialog to write fppkg-configuration files (fppkg.cfg and default) using the
+  fpcmkcfg tool that comes with fpc.
+
+  Copyright (C) 2019 Joost van der Sluis/CNOC joost@cnoc.nl
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+  Boston, MA 02110-1335, USA.
+}
 unit GenerateFppkgConfigurationDlg;
 
 {$mode objfpc}{$H+}
@@ -58,19 +78,21 @@ type
     procedure FppkgWriteConfigButtonClick(Sender: TObject);
   private
     FCompiler: string;
+    FFppkgCfgFilename: string;
+    fLastParsedFpcPrefix: string;
+    fLastParsedFpcLibPath: string;
     procedure SetCompiler(AValue: string);
     procedure SetFppkgCfgFilename(AValue: string);
     function CheckIfWritable(Filename: string): Boolean;
-  private
-    fLastParsedFpcPrefix: string;
-    fLastParsedFpcLibPath: string;
-    FFppkgCfgFilename: string;
     function CheckFppkgQuality(APrefix: string; out LibPath, Note: string): TSDFilenameQuality;
     procedure UpdateFppkgNote;
     procedure SearchFppkgFpcPrefixCandidates;
     function CheckFpcmkcfgQuality(out Note: string): TSDFilenameQuality;
   public
+    // Filename of the Free Pascal compiler that has to be written to the
+    // configuration-files.
     property Compiler: string read FCompiler write SetCompiler;
+    // Filename of the configuration file that has to be written.
     property FppkgCfgFilename: string read FFppkgCfgFilename write SetFppkgCfgFilename;
   end;
 
@@ -258,10 +280,10 @@ begin
   {$ENDIF}
   Note := Note + Format(lisGenerateFppkgCompCfg, [FileName]) + LineEnding;
 
-  if CheckIfWritable(FileName) then
-    Msg := Msg + lisWarning + ueFileROText1 + FileName + ueFileROText2 + LineEnding;
-  if CheckIfWritable(FppkgCfgFilename) then
+  if not CheckIfWritable(FppkgCfgFilename) then
     Msg := Msg + lisWarning + ueFileROText1 + FppkgCfgFilename + ueFileROText2 + LineEnding;
+  if not CheckIfWritable(FileName) then
+    Msg := Msg + lisWarning + ueFileROText1 + FileName + ueFileROText2 + LineEnding;
 
   if Msg<>'' then
   begin
@@ -316,7 +338,7 @@ begin
     Proc := TProcessUTF8.Create(nil);
     try
 
-      Proc.Options := proc.Options + [poWaitOnExit,poUsePipes];
+      Proc.Options := proc.Options + [poNoConsole, poWaitOnExit,poUsePipes];
       // Write fppkg.cfg
       Proc.Executable := FpcmkcfgExecutable;
       proc.Parameters.Add('-V');
@@ -397,6 +419,17 @@ var
   Proc: TProcessUTF8;
   Fppkg: TFppkgHelper;
 {$ENDIF}
+
+  procedure ShowFpcmkcfgError;
+  begin
+    SetLength(Msg, Proc.Output.NumBytesAvailable);
+    if Msg <> '' then
+      begin
+      Proc.Output.Read(Msg[1], Proc.Output.NumBytesAvailable);
+      IDEMessageDialog(lisFppkgProblem, Format(lisFppkgCreateFileFailed, [GetFppkgConfigFile(False, False), Msg]), mtWarning, [mbOK])
+      end;
+  end;
+
 begin
   {$IF FPC_FULLVERSION>30100}
   try
@@ -405,13 +438,13 @@ begin
     begin
       Proc := TProcessUTF8.Create(nil);
       try
-        Proc.Options := proc.Options + [poWaitOnExit];
+        Proc.Options := proc.Options + [poWaitOnExit, poNoConsole, poUsePipes, poStderrToOutPut];
         // Write fppkg.cfg
         Proc.Executable := FpcmkcfgExecutable;
         proc.Parameters.Add('-p');
         proc.Parameters.Add('-3');
         proc.Parameters.Add('-o');
-        proc.Parameters.Add(GetFppkgConfigFile(False, False));
+        proc.Parameters.Add(FppkgCfgFilename);
         proc.Parameters.Add('-d');
         proc.Parameters.Add('globalpath='+fLastParsedFpcLibPath);
         proc.Parameters.Add('-d');
@@ -425,7 +458,7 @@ begin
         Fppkg:=TFppkgHelper.Instance;
 
         if proc.ExitStatus <> 0 then
-          IDEMessageDialog(lisFppkgProblem, Format(lisFppkgCreateFileFailed, [GetFppkgConfigFile(False, False)]), mtWarning, [mbOK])
+          ShowFpcmkcfgError
         else
           begin
           Fppkg:=TFppkgHelper.Instance;
@@ -445,7 +478,7 @@ begin
             proc.Execute;
 
             if proc.ExitStatus <> 0 then
-              IDEMessageDialog(lisFppkgProblem, Format(lisFppkgCreateFileFailed, [CompConfigFilename]), mtWarning, [mbOK]);
+              ShowFpcmkcfgError
             end;
           end;
 
@@ -462,8 +495,7 @@ begin
   fLastParsedFpcPrefix := '';
   UpdateFppkgNote;
   {$ENDIF}
-
-  if CheckFppkgConfiguration(Msg)<>sddqCompatible then
+  if CheckFppkgConfiguration(FFppkgCfgFilename, Msg)<>sddqCompatible then
   begin
     IDEMessageDialog(lisFppkgProblem, Format(lisFppkgWriteConfFailed, [Msg]),
       mtWarning, [mbOK]);
@@ -480,7 +512,7 @@ begin
   begin
     Filename := ExpandFileNameUTF8(Filename);
     if FileExistsUTF8(Filename) then
-      Result := not FileIsWritable(FileName)
+      Result := FileIsWritable(FileName)
   end;
 end;
 

commit 24bee3cab2d5afd8c072b2c7c83c10de160d832b

IDE: Fixed memory-leak introduced in r60944
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@61003 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/environmentopts.pp
@@ -1848,6 +1848,7 @@ begin
   FreeAndNil(FDbgConfigStore);
   FreeAndNil(FXMLCfg);
   FreeAndNil(FLastOpenPackages);
+  FreeAndNil(FFppkgConfigFileHistory);
   inherited Destroy;
 end;
 